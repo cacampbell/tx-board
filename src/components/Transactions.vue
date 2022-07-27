2017420655b049ddf285e9b5d2b940fd46fb8c2a
Chris Campbell <chris@launchbadge.com>
2022-07-26 17:05:03 -0700

    feat: transfer hbar, staking
    Signed-off-by: Chris Campbell <chris@launchbadge.com>

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import { AccountId, Client, TransferTransaction, Hbar } from "@hashgraph/sdk";
+import {
+  AccountId,
+  Client,
+  TransferTransaction,
+  Hbar,
+  AccountCreateTransaction,
+  AccountUpdateTransaction,
+} from "@hashgraph/sdk";
 import { Wallet } from "../ledger/wallet";
 import { useWalletStore } from "../stores/wallet";
 
 const LEDGER_TEST_ACCOUNT = "0.0.47729388";
 const walletStore = useWalletStore();
+
+async function getClient(): Promise<Client> {
+  const operator = AccountId.fromString(LEDGER_TEST_ACCOUNT);
+  const client = Client.forTestnet();
+  const publicKey = await walletStore.wallet?.getPublicKey(0);
+  const signer = walletStore.wallet?.getTransactionSigner(0);
+  client.setOperatorWith(operator, publicKey!, signer!);
+  return client;
+}
 
 function handleConnectWallet(): void {
   walletStore.wallet = new Wallet();
@@ -12,10 +28,7 @@ function handleConnectWallet(): void {
 
 async function handleVerifyAccount(): Promise<void> {
   const operator = AccountId.fromString(LEDGER_TEST_ACCOUNT);
-  const client = Client.forTestnet();
-  const publicKey = await walletStore.wallet?.getPublicKey(0);
-  const signer = walletStore.wallet?.getTransactionSigner(0);
-  client.setOperatorWith(operator, publicKey!, signer!);
+  const client = await getClient();
 
   const verifyTx = new TransferTransaction()
     .addHbarTransfer(operator, Hbar.fromTinybars(0))
@@ -28,16 +41,76 @@ async function handleVerifyAccount(): Promise<void> {
   await verifyTx.execute(client);
 }
 
-function handleCreateAccount(): void {
-  console.log("handleCreateAccount");
+async function handleCreateAccount(): Promise<void> {
+  const client = await getClient();
+
+  const createTx = new AccountCreateTransaction()
+    .setInitialBalance(Hbar.fromTinybars(16_789))
+    .setKey(client.operatorPublicKey!)
+    .setTransactionMemo("New Account!")
+    .setMaxTransactionFee(new Hbar(2))
+    .freezeWith(client);
+
+  console.log(createTx);
+  console.log(createTx.toBytes());
+
+  await createTx.execute(client);
 }
 
-function handleUpdateAccount(): void {
-  console.log("handleUpdateAccount");
+async function handleStakeNode(): Promise<void> {
+  const client = await getClient();
+  const operator = client.operatorAccountId;
+
+  const stakeTx = new AccountUpdateTransaction()
+    .setAccountId(operator!)
+    .setStakedNodeId(3)
+    .setDeclineStakingReward(false)
+    .setTransactionMemo("Stake to Node 3")
+    .setMaxTransactionFee(new Hbar(1))
+    .freezeWith(client);
+
+  console.log(stakeTx);
+  console.log(stakeTx.toBytes());
+
+  await stakeTx.execute(client);
 }
 
-function handleTransferHbar(): void {
-  console.log("handleTransferHbar");
+async function handleStakeAccount(): Promise<void> {
+  const client = await getClient();
+  const operator = client.operatorAccountId;
+
+  const stakeTx = new AccountUpdateTransaction()
+    .setAccountId(operator!)
+    .setStakedAccountId(AccountId.fromString("0.0.6189"))
+    .setDeclineStakingReward(false)
+    .setTransactionMemo("Stake to Account 6189")
+    .setMaxTransactionFee(new Hbar(1))
+    .freezeWith(client);
+
+  console.log(stakeTx);
+  console.log(stakeTx.toBytes());
+
+  await stakeTx.execute(client);
+}
+
+async function handleTransferHbar(): Promise<void> {
+  const client = await getClient();
+  const operator = client.operatorAccountId;
+
+  const transferTx = new TransferTransaction()
+    .setTransactionMemo("Transfer 10.156 Hbar to 0.0.6189")
+    .addHbarTransfer(operator!, new Hbar("-10.156"))
+    .addHbarTransfer(
+      AccountId.fromString("0.0.6189"),
+      Hbar.fromString("10.156")
+    )
+    .setMaxTransactionFee(new Hbar(2))
+    .freezeWith(client);
+
+  console.log(transferTx);
+  console.log(transferTx.toBytes());
+
+  await transferTx.execute(client);
 }
 
 function handleTransferToken(): void {
@@ -66,7 +139,8 @@ function handleBurnToken(): void {
     <Button label="Connect Wallet" @click="handleConnectWallet" />
     <Button label="Verify Account" @click="handleVerifyAccount" />
     <Button label="Create Account" @click="handleCreateAccount" />
-    <Button label="Update Account" @click="handleUpdateAccount" />
+    <Button label="Stake to Node 3" @click="handleStakeNode" />
+    <Button label="Stake to Account 6189" @click="handleStakeAccount" />
     <Button label="Transfer Hbar" @click="handleTransferHbar" />
     <Button label="Transfer Token" @click="handleTransferToken" />
     <Button label="Associate Token" @click="handleAssociateToken" />
